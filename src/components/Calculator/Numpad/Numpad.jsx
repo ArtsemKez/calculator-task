d8721a3b09db8a668a17fe50875d429e11ed3def
ArtsemKez <artsemkez@yandex.ru>
2022-08-10 16:08:50 +0300

    Took out the logic from the CustomizedButton.jsx

--- src/components/Calculator/Numpad/Numpad.jsx
+++ src/components/Calculator/Numpad/Numpad.jsx
@@ -1,35 +1,63 @@
 import React from 'react';
 import styles from './Numpad.module.css'
 import CustomizedButton from "../../UI/CustomizedButton/CustomizedButton";
+import {useDispatch, useSelector} from "react-redux";
+import {getCalculationMethodSelector, getInputValueSelector, getPrevInputSelector} from "../../../Redux/Selectors";
+import {actions} from "../../../Redux/CalculatorReducer";
 
 const Numpad = () => {
 
+        const dispatch = useDispatch()
+
+        const inputValue = useSelector(getInputValueSelector)
+        const prevInputValue = useSelector(getPrevInputSelector)
+        const calcMethod = useSelector(getCalculationMethodSelector)
+
+        const clickButton = (props) => {
+                console.log(props.currentTarget.innerText)
+
+                if (props.currentTarget.innerText >= 0 && props.currentTarget.innerText < 10) {
+                        let newValue = inputValue + String(props.currentTarget.innerText)
+                        dispatch(actions.changeInput(newValue))
+                        if(calcMethod === '='){
+                                dispatch(actions.clearPrevInput())
+                        }
+                } else {
+                        if(inputValue !== ''){
+                                if(prevInputValue === ''){
+                                        dispatch(actions.setPrevInput())
+                                        dispatch(actions.deleteInputValue())
+                                        dispatch(actions.setCalculationMethod(props.currentTarget.innerText))
+                                }else if(prevInputValue !== ''){
+                                        dispatch(actions.result())
+                                        dispatch(actions.setCalculationMethod(props.currentTarget.innerText))
+                                        dispatch(actions.deleteInputValue())
+                                }
+                        }else if(inputValue === ''){
+                                dispatch(actions.setCalculationMethod(props.currentTarget.innerText))
+                        }
+
+                }
+
+        }
+
     return (
         <div className={styles.numpad}>
-            <div className={styles.one}><CustomizedButton>1</CustomizedButton></div>
-            <div className={styles.two}><CustomizedButton>2</CustomizedButton></div>
-            <div className={styles.three}><CustomizedButton >3</CustomizedButton></div>
-            <div className={styles.four}><CustomizedButton>4</CustomizedButton></div>
-            <div className={styles.five}><CustomizedButton>5</CustomizedButton></div>
-            <div className={styles.six}><CustomizedButton>6</CustomizedButton></div>
-            <div className={styles.seven}><CustomizedButton>7</CustomizedButton></div>
-            <div className={styles.eight}><CustomizedButton>8</CustomizedButton></div>
-            <div className={styles.nine}><CustomizedButton>9</CustomizedButton></div>
-            <div className={styles.zero}><CustomizedButton>0</CustomizedButton></div>
-            <div className={styles.plus}><CustomizedButton>+</CustomizedButton></div>
-            <div className={styles.minus}><CustomizedButton>-</CustomizedButton></div>
-            <div className={styles.multiply}><CustomizedButton>*</CustomizedButton></div>
-            <div className={styles.divide}><CustomizedButton>/</CustomizedButton></div>
-            <div className={styles.equally}><CustomizedButton>=</CustomizedButton></div>
-
-
-
-
-
-
-
-
-
+            <div className={styles.one} onClick={clickButton}><CustomizedButton>1</CustomizedButton></div>
+            <div className={styles.two} onClick={clickButton}><CustomizedButton>2</CustomizedButton></div>
+            <div className={styles.three} onClick={clickButton}><CustomizedButton >3</CustomizedButton></div>
+            <div className={styles.four} onClick={clickButton}><CustomizedButton>4</CustomizedButton></div>
+            <div className={styles.five} onClick={clickButton}><CustomizedButton>5</CustomizedButton></div>
+            <div className={styles.six} onClick={clickButton}><CustomizedButton>6</CustomizedButton></div>
+            <div className={styles.seven} onClick={clickButton}><CustomizedButton>7</CustomizedButton></div>
+            <div className={styles.eight} onClick={clickButton}><CustomizedButton>8</CustomizedButton></div>
+            <div className={styles.nine} onClick={clickButton}><CustomizedButton>9</CustomizedButton></div>
+            <div className={styles.zero} onClick={clickButton}><CustomizedButton>0</CustomizedButton></div>
+            <div className={styles.plus} onClick={clickButton}><CustomizedButton>+</CustomizedButton></div>
+            <div className={styles.minus} onClick={clickButton}><CustomizedButton>-</CustomizedButton></div>
+            <div className={styles.multiply} onClick={clickButton}><CustomizedButton>*</CustomizedButton></div>
+            <div className={styles.divide} onClick={clickButton}><CustomizedButton>/</CustomizedButton></div>
+            <div className={styles.equally} onClick={clickButton}><CustomizedButton>=</CustomizedButton></div>
         </div>
     );
 };
--- src/components/UI/CustomizedButton/CustomizedButton.jsx
+++ src/components/UI/CustomizedButton/CustomizedButton.jsx
@@ -1,43 +1,7 @@
 import React from 'react';
 import styles from './CustomizedButton.module.css'
-import {useDispatch, useSelector} from "react-redux";
-import {getCalculationMethodSelector, getInputValueSelector, getPrevInputSelector} from "../../../Redux/Selectors";
-import {actions} from "../../../Redux/CalculatorReducer";
 
-const CustomizedButton = (props) => {
-
-    const dispatch = useDispatch()
-
-    const inputValue = useSelector(getInputValueSelector)
-    const prevInputValue = useSelector(getPrevInputSelector)
-    const calcMethod = useSelector(getCalculationMethodSelector)
-
-    const clickButton = () => {
-        if (props.children >= 0 && props.children < 10) {
-            let newValue = inputValue + String(props.children)
-            dispatch(actions.changeInput(newValue))
-            if(calcMethod === '='){
-                dispatch(actions.clearPrevInput())
-            }
-        } else {
-            if(inputValue !== ''){
-                if(prevInputValue === ''){
-                    dispatch(actions.setPrevInput())
-                    dispatch(actions.deleteInputValue())
-                    dispatch(actions.setCalculationMethod(props.children))
-                }else if(prevInputValue !== ''){
-                    dispatch(actions.result())
-                    dispatch(actions.setCalculationMethod(props.children))
-                    dispatch(actions.deleteInputValue())
-                }
-            }else if(inputValue === ''){
-                dispatch(actions.setCalculationMethod(props.children))
-            }
-
-        }
-
-    }
-
+const CustomizedButton = ( props, {clickButton}) => {
 
     return (
         <button {...props}
